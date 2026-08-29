3d62cfda37dbe13660975a3028b5a87b195cf013
Kamalesh <kamalesh@axtr.in>
2026-08-29 12:53:38 +0530

    Removed the hardcoded url

--- backend/app/main.py
+++ backend/app/main.py
@@ -15,7 +15,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[settings.frontend_url.rstrip("/")],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
